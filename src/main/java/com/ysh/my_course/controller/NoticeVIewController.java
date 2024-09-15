7d317842c4255e5b5663ac35fa1ce23924e01cda
ysh <shyoo1023@gmail.com>
2024-09-15 14:27:21 +0900

    notice detail view

--- src/main/java/com/ysh/my_course/controller/NoticeVIewController.java
+++ src/main/java/com/ysh/my_course/controller/NoticeVIewController.java
@@ -4,11 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ysh.my_course.dto.notice.ResponseNoticeDto;
 import com.ysh.my_course.service.NoticeService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,13 +24,33 @@ public class NoticeVIewController {
 	
 	@GetMapping("/notice")
 	public String noticeList(@RequestParam(name="page", defaultValue="0", required=false) int pageNo, @RequestParam(name="noticeName", required=false) String noticeName, Model model) {
-		log.info(String.format("Called noticeListView : [ pageNo : %d ]", pageNo));
+		log.info(String.format("Called noticeListView : [ pageNo : %d, noticeName : %s ]", pageNo, noticeName));
 		
 		try {
 			Page<ResponseNoticeDto> list = noticeService.getNoticeList(pageNo, noticeName);
 			model.addAttribute("list", list);
 			model.addAttribute("noticeName", noticeName);
 			return "notice/noticeList";			
+			
+		}catch(Exception e) {
+			log.error(e.getMessage());
+			return "error/error_500";
+		}
+	}
+	
+	@GetMapping("/notice/{noticeId}")
+	public String getNotice(@PathVariable(name="noticeId") Long noticeId, HttpServletRequest request, Model model) {
+		log.info(String.format("Called getNotice : [ noticeId : %d ]", noticeId));
+		
+		HttpSession session = request.getSession();
+		
+		
+		try {
+			ResponseNoticeDto notice = noticeService.getNotice(noticeId);
+			model.addAttribute("userAuth", session.getAttribute("userAuth"));
+			model.addAttribute("notice", notice);
+			
+			return "notice/detail";
 			
 		}catch(Exception e) {
 			log.error(e.getMessage());
--- src/main/webapp/WEB-INF/views/notice/detail.jsp
+++ src/main/webapp/WEB-INF/views/notice/detail.jsp
@@ -1,12 +1,47 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<fmt:parseDate value="${notice.createdDt }" pattern="yyyy-MM-dd'T'HH:mm" var="parsedDatetime" type="both" />
+<fmt:formatDate pattern="yyyy년 MM월 dd일" value="${parsedDatetime }" var="createdDt"/>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>MyCourse</title>
+<link rel="stylesheet" href="/css/notice_detail.css" />
 </head>
 <body>
-공지사항 뷰
+<div class="notice_detail">
+	<div class="wrapper">
+		<jsp:include page="../common/header.jsp" />
+		<hr />
+		<nav>
+			<img src="/images/arrow/arrow_back.svg" alt="go_back" onClick="goBack()"/>
+		</nav>
+		<div class="detail_container">
+			<div class="title">
+				<span>${notice.title}</span>
+				<span>${notice.username }</span>
+			</div>
+			<div class="createdDate">
+				<p>${createdDt }</p>
+			</div>
+			<hr style="margin-top: 35px;"/>
+			<div class="notice_content">
+				${notice.content }
+			</div>
+		</div>
+		
+	</div>
+</div>
 </body>
+<script type="text/javascript">
+const goBack = () => {
+	let referrer = document.referrer;
+	location.replace(referrer);
+}
+</script>
 </html>
