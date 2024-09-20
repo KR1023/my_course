11c1f0a6be95bf80a19e975676676c7647afd564
ysh <shyoo1023@gmail.com>
2024-09-20 12:39:37 +0900

    remove duplicated

--- src/main/java/com/ysh/my_course/MyCourseApplication.java
+++ src/main/java/com/ysh/my_course/MyCourseApplication.java
@@ -5,14 +5,20 @@ import java.io.File;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
-public class MyCourseApplication {
+public class MyCourseApplication extends SpringBootServletInitializer{
 
 	@Value("${IMG.PATH}")
 	private String uploadDir;
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyCourseApplication.class);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MyCourseApplication.class, args);
